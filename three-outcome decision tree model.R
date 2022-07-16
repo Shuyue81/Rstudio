5342b6db4c3936fdcc7c2563c4a1ca7ea59da695
Shuyue81 <109419458+Shuyue81@users.noreply.github.com>
2022-07-16 20:40:37 +0800

    Add files via upload

---
+++ three-outcome decision tree model.R
@@ -0,0 +1,55 @@
+# step1:导入数据
+rm(list=ls())
+setwd("F:/DT")
+df=read.csv("2021_080910_time.csv")
+head(df)
+table(df$shutter)
+shuffle.index=sample(1:nrow(df))
+class(df)
+
+#step2:筛选特征
+varlist=c("In_Building","Light_I","Desk_I","Desk_T","Desk_H","wind_speed","Light_on_off",
+          "wind_direction","temperature","humidity","radiation","PM10","PM25","shutter")
+df=df[c(varlist)]
+df=na.omit(df)
+df=as.data.fram(lapply(df,as.numeric))
+df$shutter=cut(df$shutter, breaks = c(0,0.4,0.6,1.1),labels = c('<0.5','=0.5','>0.5'),
+               include.lowest = T,ordered_result = T)
+
+#step3:创建训练集和测试集
+library(caret)
+train.index = createDataPartition(df$shutter, p = 0.9, list= FALSE)
+train.data = df[train.index, ]
+test.data = df[-train.index, ]
+prop.table(table(train.data$shutter)) #目标变量的分布
+
+#step4:构建决策树
+#install.packages("rpart")
+#install.packages("rpart.plot")
+library(rpart)
+library(rpart.plot)
+tree.fit = rpart(shutter~.,data=train.data,method='class')
+par(mfrow=c(1,1))
+rpart.plot(tree.fit, extra= 100)
+
+#step5:模型预测
+predict.unseen = predict(tree.fit, test.data, type= 'class')
+
+#step6:模型评价
+table.mat <- table(test.data$shutter, predict.unseen)
+table.mat
+accuracy.test <- sum(diag( table.mat)) / sum( table.mat)
+print(paste( 'Accuracy for test', accuracy.test))
+
+#step7:模型调优
+AccuracyTune <- function(fit){
+  predict.unseen <- predict(fit, test.data, type= 'class')
+  table.mat <- table(test.data$shutter, predict.unseen)
+  accuracy.test <- sum(diag( table.mat)) / sum( table.mat)
+  accuracy.test
+}
+control = rpart.control(minsplit = 100, minbucket = 10,maxdepth =5,cp=0.005)
+tune.fit <- rpart(shutter ~ ., data = train.data, method = 'class', control = control)
+print(paste('Accuracy for test', AccuracyTune(tune.fit)))
+par(mfrow=c(1,1))
+rpart.plot(tune.fit, extra= 100)
